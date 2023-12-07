9af901aeb2167695cbc9ff42ca57bd5a66dcaf51
playoffthecuff <jmobile@bk.ru>
2023-12-07 05:25:07 +0300

    feat: add burger

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,3 +1,16 @@
 console.log('Hello buddy!');
+document.querySelectorAll('video').forEach(v => { v.setAttribute('pip', 'false'); }); //Yandex-Browser pip-off
 const burgerButton = document.getElementById('burger-button');
-document.querySelectorAll('video').forEach(v => { v.setAttribute('pip', 'false'); }) //Yandex-Browser pip-off
+const popUp = document.querySelector('.pop-up');
+const burgerIconAbove = document.getElementById('above');
+const burgerIconBelow = document.getElementById('below');
+const burgerLinks = document.querySelectorAll('.collapse-burger');
+const fromBurgerToCross = (event) => {
+        burgerIconAbove.classList.toggle('to-cross-above');
+        burgerIconBelow.classList.toggle('to-cross-below');
+        popUp.classList.toggle('pop-up__visible');
+}
+burgerButton.addEventListener('click', (fromBurgerToCross));
+burgerLinks.forEach(item => {
+    item.addEventListener('click', (fromBurgerToCross))
+});
