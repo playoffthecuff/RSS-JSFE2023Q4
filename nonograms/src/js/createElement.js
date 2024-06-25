c1db282c989db77880930b933904939d76d5a2c2
playoffthecuff <jmobile@bk.ru>
2024-06-25 21:15:52 +0300

    build: nonograms, rss-puzzle - update base

--- nonograms/src/js/createElement.js
+++
@@ -1,9 +0,0 @@
-export const createElement = (type, ...classes) => {
-  const elem = document.createElement(type);
-  if (classes.length !== 0) {
-    for (let i = 0; i < classes.length; i += 1) {
-      if (classes[i]) elem.classList.add(classes[i]);
-    }
-  }
-  return elem;
-  }
--- nonograms/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../styles/main.scss';
-import { modalContainer } from './modalWindow.js';
-import { main } from './createLayout.js';
-
-const body = document.body;
-body.append(modalContainer);
-
-body.append(main);
